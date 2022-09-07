076914e986b0d731b635392188bf1df7c95f326b
Adam Read <adamread@google.com>
2022-09-07 13:41:42 +0200

    First commit of basic functions.
    tagspeed_audit.ts has functions to open the target window and set up
    the listeners to be able to process messages from the target.
    
    gtm_tag.js is the tag that will be downloaded using an html tag in GTM.

---
+++ src/gtm_tag.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * @fileoverview Loaded by GTM on the page to be analysed to send Core Web
+ * Vitals metrics to the opener.
+ *
+ * The [web-vitals library](https://github.com/GoogleChrome/web-vitals) must be
+ * loaded before this tag is.
+ *
+ */
+
+// this needs to be provided, as opener.origin isn't available
+// TODO (b/238839377): find a way to fill in the platform's origin
+const PLATFORM_ORIGIN = 'FILL ME IN';
+
+/**
+   postToAnalysisPlatform uses postMessage to send the given metric to the
+   analysis platform.
+
+   It is assumed that the analysis platform is the page's opener.
+
+   @param {!webVitals.Metric} metric The metric to send.
+ */
+function postToAnalysisPlatform(metric) {
+  const data = JSON.stringify(metric);
+  window.opener.postMessage(data, PLATFORM_ORIGIN);
+}
+
+// set up webVitals to send all metrics to the analysis platform
+webVitals.getTTFB(postToAnalysisPlatform);
+webVitals.getFCP(postToAnalysisPlatform);
+webVitals.getLCP(postToAnalysisPlatform);
+webVitals.getFID(postToAnalysisPlatform);
+webVitals.getCLS(postToAnalysisPlatform);
---
+++ src/tagspeed_audit.ts
@@ -0,0 +1,62 @@
+/**
+ * @fileoverview Main file for the tag-speed audit platform. All of the code
+ * directly related to the tool's web page is collected here.
+ */
+
+import {Metric} from 'web-vitals/src/types';
+import 'gapi';
+
+/*
+ * This is global so that we can prevent multiple tests being run at once,
+ * which is not supported.
+ */
+let TARGET_WINDOW: Window | null = null;
+
+const CLIENT_ID = '';
+const GTM_API_KEY = '';
+
+
+
+/**
+ * Sets up a listener for window.postMessage messages.
+ * The messages listened for will only be processed if they originate from the
+ * target origin, provided by the origin parameter.
+ *
+ * @param origin The origin of the site under analysis.
+ */
+function createMessageListener(origin: string) {
+  function messageEventListener(event: MessageEvent) {
+    if (event.origin !== origin) return;
+    const metric = JSON.parse(event.data) as Metric;
+    const resultsTable = document.getElementById('results-table');
+    if (!resultsTable) return;
+    resultsTable.innerHTML = `<tr><td>${metric.name}</td><td>${metric.delta}</td></tr>`;
+  }
+  removeEventListener('message', messageEventListener);
+  addEventListener('message', messageEventListener);
+}
+
+/**
+ * Opens a new window with the page to be audited and creates
+ * the event listener for the messages being posted back to the platform.
+ */
+function openTargetWindow() {
+  const urlInput = document.getElementById('url-input') as HTMLInputElement;
+  if (!urlInput) return;
+  const targetURL = new URL(urlInput.value);
+  if (TARGET_WINDOW === null || TARGET_WINDOW.closed) {
+    TARGET_WINDOW = window.open(targetURL.toString());
+    if (!TARGET_WINDOW) return;
+    TARGET_WINDOW.focus();
+    createMessageListener(targetURL.origin);
+  } else {
+    alert('Please wait for current test to finish.');
+  }
+}
+
+/** Page Initialization */
+(() => {
+  // set up the listener to open the site being audited.
+  const form = document.getElementById('url-form') as HTMLFormElement;
+  form.addEventListener('submit', openTargetWindow);
+})();
