d69ccbc752d6db6ce46d611fd22e562acc9cd025
Maria Zoe Lopez-Latorre <68270812+notmariazoe@users.noreply.github.com>
2022-10-09 13:51:35 +0100

    Webpack and JWT decoder
    Added webpack for build packaging, added dependencies for decoding the JWT token for login, and changed the signin to use a callback as exported in the bundle. Not entirely convinced about the approach in Index, but it works for now to test the method.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,30 @@
-console.log('Try npm run lint/fix!');
+// console.log('Try npm run lint/fix!');
 
-const longString =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+// const longString =
+//   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
 
-const trailing = 'Semicolon';
+// const trailing = 'Semicolon';
 
-const why = 'am I tabbed?';
+// const why = 'am I tabbed?';
 
-export function doSomeStuff(
-  withThis: string,
-  andThat: string,
-  andThose: string[]
-) {
-  //function on one line
-  if (!andThose.length) {
-    return false;
-  }
-  console.log(withThis);
-  console.log(andThat);
-  console.dir(andThose);
-  return;
-}
-// TODO: more examples
+// export function doSomeStuff(
+//   withThis: string,
+//   andThat: string,
+//   andThose: string[]
+// ) {
+//   //function on one line
+//   if (!andThose.length) {
+//     return false;
+//   }
+//   console.log(withThis);
+//   console.log(andThat);
+//   console.dir(andThose);
+//   return;
+// }
+// // TODO: more examples
+
+import { handleUserCredentialResponse } from "./user-controller";
+
+module.exports = {
+  handleCredentialResponse: handleUserCredentialResponse
+};
