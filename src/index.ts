50f323d5d5b5f9295de4ddf5c0db8bdcd3101e7d
Maria Zoe Lopez-Latorre <68270812+notmariazoe@users.noreply.github.com>
2022-10-10 12:50:21 +0100

    Updated index (html and ts)
    Added Code license and file overview to TS to reflect current usage (previous tests commented out and moved to the bottom, in case they're still necessary)
    
    Added comment to HTML to reflect doubt about current approach.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Entry point for exposing methods from the bundle to the views
+ * and orchestration if required.
+ */
+
+import { handleUserCredentialResponse } from "./controllers/user-controller";
+
+module.exports = {
+  handleCredentialResponse: handleUserCredentialResponse
+};
+
 // console.log('Try npm run lint/fix!');
 
 // const longString =
@@ -24,9 +51,3 @@
 // // TODO: more examples
 
 // TODO: Load libraries from here rather than the html
-
-import { handleUserCredentialResponse } from "./controllers/user-controller";
-
-module.exports = {
-  handleCredentialResponse: handleUserCredentialResponse
-};
